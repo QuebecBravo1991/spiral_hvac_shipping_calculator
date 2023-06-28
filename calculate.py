a44f2d85352b8b2ce82524e72a629b8a01b6e884
Quade Barnes <quade.barnes@gmail.com>
2023-06-28 10:22:04 -0600

    created packing algorithm

--- calculate.py
+++ calculate.py
@@ -15,14 +15,17 @@ class Spiral_Pipe:
         return f"Diameter: {self.diameter} Length: {self.length} Gauge: {self.gauge}"
 
 def read_file():
-    # get the file name
-    print("Please input the name of the file you wish to caclulate from: ")
-    file_name = input()
+    while True:
+        # get the file name
+        print("Please input the name of the file you wish to caclulate from: ")
+        file_name = input()
+        # create data frame from the file
+        try:
+            data_frame = pd.read_excel(file_name, sheet_name='Sheet1')
+            return data_frame
+        except FileNotFoundError:
+            print("\nNo such file found. Did you spell it right? Is it in the right folder?\n")
 
-    # create data frame from the file
-    data_frame = pd.read_excel(file_name, sheet_name='Sheet1')
-    
-    return data_frame
 
 def collect_pipe(data_frame):
     names = data_frame['Name']
@@ -71,16 +74,93 @@ def round_up_to_5(num):
 
     return ret_val
 
+def sort_pipe(pipes):
+    ret_val = []
+
+
+    while pipes:
+        max = pipes[0]
+        
+        for pipe in pipes:
+            if (getattr(pipe, "diameter") > getattr(max, "diameter")):
+                max =  pipe
+
+
+        ret_val.append(max)
+        pipes.remove(max)
+
+    return ret_val
+
+def pack_pipe(pipes):
+    # dimensions x y z / width , height, depth
+    dimensions = [0, 0, 0]
+    
+
+    level_width = 0
+    layer_height = 0
+
+    while pipes:
+        current_diameter = getattr(pipes[0], "diameter")
+        current_length = getattr(pipes[0], "length")
+        
+        if (level_width + current_diameter <= TRAILER_WIDTH):
+            level_width += current_diameter
+            
+            if layer_height == 0:
+                layer_height = current_diameter
+            if dimensions[1] == 0:
+                dimensions[1] = current_diameter
+            if dimensions[0] < level_width:
+                dimensions[0] = level_width
+            if dimensions[2] == 0:
+                dimensions[2] = current_length
+        elif (layer_height + current_diameter <= TRAILER_HEIGHT):
+            layer_height += current_diameter
+            if layer_height > dimensions[1]:
+                dimensions[1] = layer_height
+            level_width = current_diameter
+        elif (dimensions[2] + current_length <= TRAILER_DEPTH):
+            if dimensions[2] % 10 != 0:
+                dimensions[2] += 5
+            else :
+                dimensions[2] += current_length
+            layer_height = 0
+            level_width = current_diameter
+        else :
+            raise Exception("Looks like you need more than one trailer! This feature is currently not supported!")
+
+        greatest_nestable_diameter = current_diameter
+        
+        nestable = True
+
+        while nestable:
+            found_nestable_pipe = False
+            for pipe in pipes:
+                if getattr(pipe, "diameter") < greatest_nestable_diameter:
+                    greatest_nestable_diameter = getattr(pipe, "diameter")
+                    pipes.remove(pipe)
+                    found_nestable_pipe = True
+                    break
+            if found_nestable_pipe == False:
+                nestable = False
+    
+        pipes.pop(0)
+
+    print("The width is: " + str(dimensions[0]) + "\" The height is: " + str(dimensions[1])  + "\" The depth is " + str(dimensions[2]) + "\'")
+
+
 def print_pipe(pipes):
-    print("The following pipe(s) were found: ")
+    print("\n The following pipe(s) were found: ")
 
     for pipe in pipes:
         print(pipe)
+
+    print()
 
 
 
 data_frame = read_file()
 pipes = collect_pipe(data_frame)
+pipes = sort_pipe(pipes)
 print_pipe(pipes)
-
-
+pack_pipe(pipes)
